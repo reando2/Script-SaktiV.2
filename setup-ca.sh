8a63002ca530682230e5bf8042d358b9668a6377
reando2 <halogaes113@gmail.com>
2026-03-08 04:55:38 -0400

    error fix on setup-ca

--- setup-ca.sh
+++ setup-ca.sh
@@ -1,6 +1,16 @@
 read -p  "Enter path certificate: "  path_cert
+if [ -z "$path_cert" ]; then
+	echo "Path cannot be empty!, cancelled"
+	exit 1
+fi
 
-sed -i 's/.demoCA/$path_cert/g' /etc/ssl/openssl.cnf
+read -p  "Enter Common Name for CA: " cn
+if [ -z "$cn" ]; then
+	echo "Common Name cannot be empty!, cancelled"
+	exit 1
+fi
+
+sed -i '82s|\./demoCA|$path_cert|' /etc/ssl/openssl.cnf
 sed -i 's/# copy_extensions = copy/copy_extensions = copyall/g' /etc/ssl/openssl.cnf
 sed -i 's/policy_match/policy_anything/g' /etc/ssl/openssl.cnf
 
@@ -11,4 +21,5 @@ mkdir -p "$path_cert/private"
 touch "$path_cert/index.txt"
 echo 01 >> "path_certs/serial"
 
-openssl req -new -nodes -x509 -out "$path_cert/cacert.pem" -keyout "$path_cert/private/cakey.pem" -days 3650 
+openssl req -new -nodes -x509 -out "$path_cert/cacert.pem" -keyout "$path_cert/private/cakey.pem" -days 3650 \
+	-subj "/C=ID/CN=$cn"
